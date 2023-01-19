7a2c5859ad6d573b2d887e509ad1b82a2b049cdf
Bi Nguyen <binguyen@Bis-MacBook-Pro.k4connect.private>
2023-01-19 13:47:47 -0500

    Update layout

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,13 @@
 import Nav from "./nav";
+import Footer from "../components/footer";
+import styles from "../styles/Layout.module.css";
 
-const Layout = ({ children, categories, seo }) => (
-  <>
-    <Nav categories={categories} />
+const Layout = ({ children }) => (
+  <div className={styles.layout}>
+    <Nav />
     {children}
-  </>
+    <Footer />
+  </div>
 );
 
 export default Layout;
