3031503c21ff1f96c6240d2b1f32491aa9b4ea8b
Bi Nguyen <binguyen@bis-macbook-pro.lan>
2023-03-24 17:07:13 -0400

    Fix timeline. Add google photos

--- components/timeline.js
+++ components/timeline.js
@@ -17,13 +17,33 @@ const Timeline = ({ events, pastEventPhotos }) => {
 
                 const eventState = (new Date(e.date)).getTime() < Date.now() ? styles.past : styles.future;
 
+                // White Sands album sharing
+                // Google photos sharing link
+                if (i === 1) {
+                    return (
+                        <div key={i} className={`${styles.box} ${styles.left} ${eventState}`}>
+                            <div className={styles.content}>
+                                <h2>{e.location}</h2>
+                                <h3>{e.date}</h3>
+                                <ReactMarkdown>{e.description}</ReactMarkdown>
+                                <Button className={styles.button}>
+                                    <a href={process.env.GOOGLE_PHOTOS} target='_blank'>
+                                        Pictures!
+                                    </a>
+                                </Button>
+                            </div>
+                        </div>
+                    )
+                }
+
+
                 // Even IDs are on the left side of the timeline
                 if (i === 0 || i % 2 == 0) {    
 
                     // Past events will show a button and modal for pictures
                     if (eventState === styles.past) {
                         return (
-                            <div key={i} className={`${styles.box} ${styles.right} ${eventState}`}>
+                        <div key={i} className={`${styles.box} ${styles.right} ${eventState}`}>
                             <div className={styles.content}>
                                 <h2>{e.location}</h2>
                                 <h3>{e.date}</h3>
@@ -52,14 +72,14 @@ const Timeline = ({ events, pastEventPhotos }) => {
                 } else {
                     // Odd IDs are on the right side of the timeline
                     // Past events will show a button and modal for pictures
-                    if (eventState === styles.past) {
+                    if (eventState === styles.past && i !== 1) {
                         return (
                             <div key={i} className={`${styles.box} ${styles.left} ${eventState}`}>
                                 <div className={styles.content}>
                                     <h2>{e.location}</h2>
                                     <h3>{e.date}</h3>
                                     <ReactMarkdown>{e.description}</ReactMarkdown>
-                                    <Button onClick={() => setModalShow(true)}>Pictures!</Button>
+                                    <Button className={styles.button} onClick={() => setModalShow(true)}>Pictures!</Button>
                                     <Modal
                                         show={modalShow}
                                         onHide={() => setModalShow(false)}
